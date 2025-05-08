1ca27ed283bf9509ebac045fa54102ed8ab62d52
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-08 14:52:55 -0500

    feat: Create PersonalInfo component to display user information

---
+++ src/components/molecules/PersonalInfo/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { SubTitle2 } from "@/components/atoms/Titles";
+import { TextRegular2 } from "@/components/atoms/Titles";
+export default function Index() {
+  return (
+    <div>
+        
+        <SubTitle2 text='Maritza Tabarez' />
+        <TextRegular2 text='Software Engineer student' />
+    </div>
+  )
+}
