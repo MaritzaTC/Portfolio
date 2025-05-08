2bb71a52875fbd6981a7a6817d44ae576a5658eb
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-08 14:53:01 -0500

    feat: Implement ProgressIndicator component for visual progress representation

---
+++ src/components/molecules/ProgressIndicator/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { TextRegular2 } from "@/components/atoms/Titles";
+const Index = ({
+  title,
+  porcentage,
+}: {
+  title: string;
+  porcentage: number;
+}) => {
+  return (
+    <div>
+      <div className="flex justify-between items-center mb-2">
+        <TextRegular2 text={title}></TextRegular2>
+        <TextRegular2 text={`${porcentage}%`}></TextRegular2>
+      </div>
+      <div className="w-full border-amber-400 rounded-full h-1.5 mb-4 ">
+        <div
+          className="bg-yellow-500 h-1.5 rounded-full transition-all duration-500"
+          style={{ width: `${porcentage}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Index;
