1bc70f13155cb6c7b09891b06faf09a2c34d016d
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-08 14:52:09 -0500

    feat: Add icon components and image for icons

---
+++ src/components/atoms/Icons/index.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Icon } from '@iconify/react';
+import Image from 'next/image';
+
+const IconLinks = ({ icon }: { icon: string }) => {
+    return (
+        <div className="bg-[#FFB400] rounded-full w-12 h-12 flex justify-center items-center " >
+              <Icon icon={`${icon}`} width='28' height='28' className='text-black'   />
+        </div>
+    );
+};  
+
+
+const IconCard1 = ({ icon }: { icon: string }) => {
+    return (
+        <div className="  flex justify-center items-center">
+              <Icon icon={`${icon}`} width='68' height='68' className='text-[#FFB400]' />
+        </div>
+    );
+}
+const IconCard2 = ({ icon }: { icon: string }) => {
+    return (
+        <div className="  flex justify-center items-center">
+              <Icon icon={`${icon}`} width='74' height='74' className='text-[#FFB400]' />
+        </div>
+    );
+}
+const IconCard3 = ({ icon }: { icon: string }) => {
+    return (
+        <div className="  flex justify-center items-center">
+              <Icon icon={`${icon}`} width='78' height='78' className='text-[#FFB400]' />
+        </div>
+    );
+}
+const IconSkill = () => {
+    return (
+        <Image
+        src="/Icons.png"
+        alt="Check mark"
+        width={15}
+        height={15}
+      />
+    );
+}
+export {IconLinks, IconCard1,IconCard2,IconCard3,IconSkill};
