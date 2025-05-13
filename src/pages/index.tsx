a73b4031c0367a69d8465df50f1cde45ce00d9de
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-13 11:19:10 -0500

    feat: Refactor Home component layout and enhance structure with additional components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
-import PanelLeft from '@/components/organisms/PanelLeft';
-
+import React from "react";
+import PanelLeft from "@/components/organisms/PanelLeft";
+import SocialSideBar from "@/components/molecules/SocialSideBar";
+import GeneralInformation from "@/components/molecules/GeneralInformation";
+import CardEducation from "@/components/organisms/CardEducation";
+import CardPersonal from "@/components/organisms/CardPersonal";
+import CardKnowledge from "@/components/organisms/CardKnowledge";
 export default function Home() {
   return (
-   <div className='bg-white flex flex-col  '>
-  <PanelLeft></PanelLeft>
-   </div>
+    <div className="bg-[#F0F0F6] ">
+      {/* <SocialSideBar></SocialSideBar>
+   <PanelLeft></PanelLeft> */}
+      <div className="flex flex-col items-center justify-center min-h-screen bg-[#F0F0F6]">
+        <CardPersonal></CardPersonal>
+        <CardKnowledge></CardKnowledge>
+        <GeneralInformation
+          title="Education"
+          description="Education has always been my gateway to growth. I strive to acquire knowledge that not only sharpens my technical abilities but also contributes to solving problems that matter in today's technology-driven world."
+        />
+        <CardEducation></CardEducation>
+      </div>
+    </div>
   );
-};
+}
