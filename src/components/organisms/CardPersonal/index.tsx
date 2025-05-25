b3cb12e2b9ec38781e6e2876c953e5a11f4044e6
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-24 22:46:22 -0500

    feat: Refactor CardPersonal component layout and improve structure

--- src/components/organisms/CardPersonal/index.tsx
+++ src/components/organisms/CardPersonal/index.tsx
@@ -2,15 +2,24 @@
 import React from 'react'
 import { Img2 } from '../../atoms/Img/index'
 import { TextTitle, TextRegular, TextTitle2 } from '../../atoms/Titles/index'
+import { HireMe } from '@/components/atoms/Button'
 export default function index() {
   return (
-    <div className='w-[970px]  h-[467px] bg-white flex flex-row items-center justify-center pt-8 pr-8 pl-8' >
+    <div className='w-full max-w-[970px] bg-white  flex flex-row justify-between '>
+    <div className='sm:px-12 py-8' >
       <div className='flex flex-col  w-full h-full justify-center '>
         <TextTitle text="I'm Maritza Tabarez"></TextTitle>
         <TextTitle2 text="System engineers"></TextTitle2>
         <TextRegular text="I am passionate about technology, with a strong interest in programming and developing innovative solutions. I enjoy tackling challenges that allow me to continuously learn, enhance my technical skills, and contribute to impactful projects."></TextRegular>
+        <HireMe></HireMe>
       </div>
-      <Img2></Img2>
+      
+      </div> 
+    
+
+     <Img2 />
+
+
     </div>
   )
 }
