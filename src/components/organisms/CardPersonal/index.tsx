bb5e1f06534484e8ccf27998428950956110bc3a
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-25 15:29:37 -0500

    feat: Add dialog functionality to HireMe button for freelance inquiries

--- src/components/organisms/CardPersonal/index.tsx
+++ src/components/organisms/CardPersonal/index.tsx
@@ -1,9 +1,12 @@
 
-import React from 'react'
+import React, { useState } from 'react'
 import { Img2 } from '../../atoms/Img/index'
 import { TextTitle, TextRegular, TextTitle2 } from '../../atoms/Titles/index'
 import { HireMe } from '@/components/atoms/Button'
+import { Dialog } from '@/components/molecules/Dialog';
 export default function index() {
+   // eslint-disable-next-line react-hooks/rules-of-hooks
+   const [showDialog, setShowDialog] = useState(false);
   return (
     <div className='w-full max-w-[970px] bg-white  flex flex-row justify-between '>
     <div className='sm:px-12 py-8' >
@@ -11,13 +14,18 @@ export default function index() {
         <TextTitle text="I'm Maritza Tabarez"></TextTitle>
         <TextTitle2 text="System engineers"></TextTitle2>
         <TextRegular text="I am passionate about technology, with a strong interest in programming and developing innovative solutions. I enjoy tackling challenges that allow me to continuously learn, enhance my technical skills, and contribute to impactful projects."></TextRegular>
-        <HireMe></HireMe>
+        <HireMe onOpen={() => setShowDialog(true)} />
+      <Dialog isOpen={showDialog} onClose={() => setShowDialog(false)}
+          titleText="Hire Me"
+          description='I am available for freelance projects and collaborations. If you are interested in working together, please feel free to reach out to me. I would be happy to discuss your project and how I can contribute to its success.' link={''}        />
       </div>
       
       </div> 
     
-
+<div className='sm:flex flex-col items-center justify-center w-full h-full'>
      <Img2 />
+</div>
+
 
 
     </div>
