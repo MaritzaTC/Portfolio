1e85fcd26e9c7b7cfe33e1c8835cab47f1531f1f
MaritzaTC <maritza.tabarezc@udea.edu.co>
2025-05-25 15:29:40 -0500

    feat: Implement Swiper for portfolio display and add dialog for project details

--- src/components/organisms/CardPortfolio/index.tsx
+++ src/components/organisms/CardPortfolio/index.tsx
@@ -1,58 +1,118 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/effect-coverflow';
+import SwiperCore, { Pagination, EffectCoverflow } from 'swiper';
+import { LearnMore } from '@/components/atoms/Button';
 import { Img3 } from '@/components/atoms/Img';
 import { SubTitle4, TextRegular2 } from '@/components/atoms/Titles';
-import GeneralInformation from '@/components/molecules/GeneralInformation'
+import { Dialog } from '@/components/molecules/Dialog';
+import GeneralInformation from '@/components/molecules/GeneralInformation';
+import { useState } from 'react';
 
-import React from 'react'
+// eslint-disable-next-line react-hooks/rules-of-hooks
+SwiperCore.use([Pagination, EffectCoverflow]);
 
 const portfolio = [
   {
-    image: "/Image.png",
-    title: "How to make web tempates",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna ",
-    details: "I have designed user-friendly interfaces for mobile apps and websites, focusing on enhancing user experience and visual appeal.",
-    github: ""
+    image: "/poke.png",
+    title: "Pokémon Web App",
+    description: "This project is an interactive web application that allows users to explore Pokémon using the PokeAPI. ",
+    details: "Users can search and view details of Pokémon, including their types, stats, evolutions, and more. The application uses JavaScript to fetch data from the API and display it in an attractive user interface.",
+    github: "GitHub: https://github.com/20251-Ingenieria-WEB/taller-html-Maritza-Tabarez-Cardenas.git"
   },
   {
-    image: "/Image.png",
-    title: "How to make web tempates",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna ",
-    details: "I have designed user-friendly interfaces for mobile apps and websites, focusing on enhancing user experience and visual appeal.",
-    github: ""
+    image: "/serenity.png",
+    title: "Serenity BDD",
+    description: "The project is focused on end-to-end test automation using Serenity BDD as the main framework for executing the tests.",
+    details: "Serenity BDD provides a structured approach to testing with detailed reporting, enabling a clear understanding of test results and application flows.\n The tests are designed to cover key aspects such as:\n User Authentication: Verifying that the login processes work correctly.\n User Registration: Ensuring that a user can be successfully created. ",
+    github: "GitHub: https://github.com/MaritzaTC/Automation-Auth-Calidad-E2E.git"
   },
   {
-    image: "/Image.png",
-    title: "How to make web tempates",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna ",
-    details: "I have designed user-friendly interfaces for mobile apps and websites, focusing on enhancing user experience and visual appeal.",
-    github: ""
-  },
-  {
-    image: "/Image.png",
-    title: "How to make web tempates",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae nulla diam in ac dictum a urna ",
-    details: "I have designed user-friendly interfaces for mobile apps and websites, focusing on enhancing user experience and visual appeal.",
-    github: ""
+    image: "/androi.png",
+    title: "User interface (GUI) ",
+    description: "A graphical user interface (GUI) is developed for Android. Using Jetpack Compose ",
+    details: "This application implements two forms using Jetpack Compose to collect the user's personal and contact information. The application supports multiple languages, screen orientations, and properly handles the virtual keyboard.",
+    github: "GitHub: https://github.com/MaritzaTC/LabsCM20251-Gr02/tree/main/Lab120251-Gr02"
   },
 ];
 
-
 export default function Index() {
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const handleOpen = (item) => {
+    setSelectedItem(item);
+  };
+
+  const handleClose = () => {
+    setSelectedItem(null);
+  };
+
   return (
-    <div>
-      <div className='flex items-center justify-center'><GeneralInformation
-        title="Portfolio"
-        description="This section showcases the projects I have developed as part of my academic training and personal growth. Each project reflects my progress in programming, design, and system development, demonstrating both technical and problem-solving skills."
-      />
+    <div className="w-full">
+      <div className='flex items-center justify-center'>
+        <GeneralInformation
+          title="Portfolio"
+          description="This section showcases the projects I have developed as part of my academic training and personal growth. Each project reflects my progress in programming, design, and system development, demonstrating both technical and problem-solving skills."
+        />
       </div>
-      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
-        {portfolio.map((item) => (
-          <div key={item.title} className="flex flex-col items-center justify-center">
-            <Img3 img={item.image}></Img3>
-            <SubTitle4 text={item.title}></SubTitle4>
-            <TextRegular2 text={item.description}></TextRegular2>
+<div className="w-full max-w-[1000px] mx-auto">
+  <Swiper
+    grabCursor={true}
+    centeredSlides={true}
+    slidesPerView={'auto'}
+    spaceBetween={72}
+    pagination={{ clickable: true }}
+    effect={'coverflow'}
+    coverflowEffect={{
+      rotate: 0,
+      stretch: 0,
+      depth: 100,
+      modifier: 2.5,
+    }}
+    breakpoints={{
+    640: { // sm
+      spaceBetween: 32,
+    },
+    768: { // md
+      spaceBetween: 48,
+    },
+    1024: { // lg
+      spaceBetween: 64,
+    },
+    1280: { // xl
+      spaceBetween: 72,
+    },
+  }}
+  >
+    {portfolio.map((item) => (
+      <SwiperSlide
+        key={item.title}
+       className="!w-[260px] !h-[400px] sm:!w-[280px] sm:!h-[420px] md:!w-[300px] md:!h-[440px] lg:!w-[310px] lg:!h-[474px]"
+      >
+        <div className="flex flex-col items-center justify-start bg-white">
+          <div className=''>   <Img3 img={item.image} />
           </div>
-        ))}
-      </div>
+       
+          <div className="p-4">
+            <SubTitle4 text={item.title} />
+            <TextRegular2 text={item.description} />
+            <LearnMore onOpen={() => handleOpen(item)} />
+          </div>
+        </div>
+      </SwiperSlide>
+    ))}
+  </Swiper>
+</div>
+      {selectedItem && (
+        <Dialog
+          isOpen={true}
+          onClose={handleClose}
+          titleText="Hire Me"
+          description={selectedItem.details}
+          link={selectedItem.github}
+        />
+      )}
     </div>
-  )
+  );
 }
